be511bfad419770f408e9b3aceed883655293f8f
Chuchundra009 <redfox161294@gmail.com>
2023-09-27 22:57:22 +0300

    add project

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const data = [
+
+    {
+      id: 1,
+      name: "Kapadokya, Goreme",
+      description: "В тот день мы встали очень рано ради такой красоты. Надо будет как-нибудь повторить",
+      age: '04.06.22',
+      image: "https://images.unsplash.com/photo-1526048598645-62b31f82b8f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+    },
+
+    {
+      id: 2,
+      name: "Bosphore, Istanbul.",
+      description: "Красивый вид на мечеть",
+      age: '05.06.22',
+      image: "https://images.unsplash.com/photo-1602941800793-78c44baca4d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80"
+    },
+
+    {
+      id: 3,
+      name: "Istanbul, Rumelihisari",
+      description: "Крепость и мост. Как интересно смотрятся прошлое и современность на одном фото",
+      age: '05.06.22',
+      image: "https://images.unsplash.com/photo-1609370850684-aec46c8a285f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80"
+    },
+
+    {
+      id: 4,
+      name: "Gümüşsuyu, Beyoğlu/İstanbul, Turkey",
+      description: "Просто оставлю это здесь",
+      age: '06.06.22',
+      image: "https://images.unsplash.com/photo-1579554370034-9aa1275e9c7b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80"
+    },
+
+    {
+      id: 5,
+      name: "Эфес",
+      description: "Античные города-моя слабость.",
+      age: '07.06.22',
+      image: "https://images.unsplash.com/photo-1596094657888-6e0a49139c1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80"
+    }
+  ]
